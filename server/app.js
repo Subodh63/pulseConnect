5072a62f201fb2c6230a29b5487616fe7c05cb0e
Soumik Saha <sahasoumik1573@gmail.com>
2024-04-24 02:39:28 +0530

    fix: Fixing CORS issue

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Origin", `${process.env.HOSTED_CLIENT_URL}`);
 	res.header(
 		"Access-Control-Allow-Methods",
 		"GET,HEAD,OPTIONS,POST,PUT,DELETE"
